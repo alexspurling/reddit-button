fedeeaad44211086b4995fcff8a61cf7f38ea65d
Alex Spurling <aspurling@qmetric.co.uk>
2015-04-02 12:30:49 +0100

    Adding index to timestamp column in db set up script

--- setupdatabase.sh
+++ setupdatabase.sh
@@ -3,7 +3,7 @@
 mysql -uroot << EOF
 create database if not exists reddit;
 use reddit;
-create table buttonclicks (timestamp TIMESTAMP, clicks INT, secondsleft TINYINT);
+create table buttonclicks (timestamp TIMESTAMP, clicks INT, secondsleft TINYINT, INDEX timestamp_index (timestamp));
 GRANT SELECT,INSERT ON buttonclicks to 'reddit'@'localhost' IDENTIFIED BY 'redditbutton';
 EOF
 
